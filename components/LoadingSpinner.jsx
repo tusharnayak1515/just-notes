7fa78790da365ffa5d3cd4921a8c929a4dd29b92
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-08-10 16:03:56 +0530

    improved styling

--- components/LoadingSpinner.jsx
+++ components/LoadingSpinner.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
+import { shallowEqual, useSelector } from 'react-redux';
 import loadingSpinner from "../public/static/images/loading.svg";
+import loadingSpinner1 from "../public/static/images/loading1.svg";
 
 import styles from "../styles/loading.module.css";
 
 const LoadingSpinner = () => {
+  const {theme} = useSelector(state=> state.default.userReducer, shallowEqual);
   return (
-    <div className={styles.loading}>
-        <Image src={loadingSpinner} alt="Loading Spinner" />
+    <div className={styles.loading} style={{backgroundColor: theme === "dark" && "#010125"}}>
+        <Image src={theme === "light" ? loadingSpinner : loadingSpinner1} alt="Loading Spinner" />
     </div>
   )
 }
